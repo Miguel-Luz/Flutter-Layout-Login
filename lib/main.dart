eb7145117b0af46766e188b614e4e1d5c27675d7
Miguel Freitas <miguel.database@gmail.com>
2020-07-14 17:27:49 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
-main(){
+main() {
   runApp(MlApp());
 }
-
 
 class MlApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
